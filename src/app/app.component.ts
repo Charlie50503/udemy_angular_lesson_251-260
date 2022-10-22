a0d1c34e3407c6d07af602e6a934f815715c2c25
charlie50503 <cate50503@gmail.com>
2022-10-22 18:51:19 +0900

    udemy_angular_lesson_256-5
    建立get請求

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.onFetchPosts()
+   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -21,9 +23,18 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
   }
 
   onClearPosts() {
     // Send Http request
   }
+
+  private fetchPosts(){
+    this.http.get("https://angular-http-request-dd100-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json")
+    .subscribe(responseData=>{
+      console.log(responseData)
+    })
+  }
+
 }

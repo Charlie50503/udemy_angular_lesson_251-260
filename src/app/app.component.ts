2c0b8e7ff3c7b3ab39c8d7d88d0575969e1081a4
dannyLiu <h152074716@gmail.com>
2022-10-29 18:29:06 +0800

    266 : 不同的錯誤處理方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,25 @@
 import { PostService } from './post.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
   constructor(private http: HttpClient, private postService: PostService) {}
 
   ngOnInit() {
+    this.errorSub = this.postService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
     this.onFetchPosts();
   }
 
@@ -44,5 +49,8 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }

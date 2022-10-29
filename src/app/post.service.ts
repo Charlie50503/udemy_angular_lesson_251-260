1a0558084ea2696e3377a30aa65303a2e314b9cd
dannyLiu <h152074716@gmail.com>
2022-10-29 19:05:07 +0800

    267 : Rx.Js 內的錯誤處理

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {}
 
   createAndStorePost(title: string, content: string) {
@@ -15,7 +18,12 @@ export class PostService {
         'https://ng-complete-guide-2a447-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
         postData
       )
-      .subscribe((responseData) => console.log(responseData));
+      .subscribe(
+        (responseData) => console.log(responseData),
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {
@@ -32,6 +40,9 @@ export class PostService {
             }
           }
           return postsArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
         })
       );
   }

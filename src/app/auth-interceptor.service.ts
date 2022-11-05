2eedd1863d211f6ab5fb1521af4334dbc62ea821
charlie50503 <cate50503@gmail.com>
2022-11-05 16:26:00 +0800

    273：刪除未使用import

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,4 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {

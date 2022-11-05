d2ce367ea15cc12f5ff45c7d4355f09026aca4ec
charlie50503 <cate50503@gmail.com>
2022-11-05 17:09:13 +0800

    274-1：更改request請求訊息

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,11 @@ import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http"
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log("request is on its way");
+    const modifiedRequest = req.clone({
+      params:null,
+      url:"some new url",
+
+    })
     return next.handle(req);
 
   }

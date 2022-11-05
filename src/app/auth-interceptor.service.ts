d3a236f15738180ed5609668a1cf2f9c669ef58f
charlie50503 <cate50503@gmail.com>
2022-11-05 17:42:00 +0800

    275：添加pipe到攔截器

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,9 +1,9 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>  {
-    console.log("request is on its way");
     req= req.clone({
       headers:req.headers.append('Auth','xyz'),
     })
